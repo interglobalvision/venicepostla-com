03134e8ddc9c0c9b28aea53b6b746a2062b044af
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-10-31 15:49:37 -0600

    scaffold page-contact

--- page-contact.php
+++ page-contact.php
@@ -2,14 +2,40 @@
 get_header();
 ?>
 
-<main id="main-content"  class="grid-row">
+<main id="main-content"  class="grid-row align-items-center">
 <?php
 if (have_posts()) {
   while (have_posts()) {
     the_post();
+
+    $address = get_post_meta($post->ID, '_igv_contact_address', true);
+    $phone = get_post_meta($post->ID, '_igv_contact_phone', true);
+    $email = get_post_meta($post->ID, '_igv_contact_email', true);
 ?>
   <article <?php post_class('grid-item item-s-12 item-m-3'); ?> id="post-<?php the_ID(); ?>">
-    <?php the_content(); ?>
+    <div class="font-uppercase"><?php the_content(); ?></div>
+<?php
+  if (!empty($address)) {
+?>
+    <h2 class="font-uppercase">Address</h2>
+    <?php echo apply_filters('the_content', $address); ?>
+<?php
+  }
+
+  if (!empty($phone)) {
+?>
+    <h2 class="font-uppercase">Telephone</h2>
+    <p><a href="tel:<?php echo preg_replace("/[^0-9]/", "", $phone); ?>"><?php echo $phone; ?></a></p>
+<?php
+  }
+
+  if (!empty($email)) {
+?>
+    <h2 class="font-uppercase">Email</h2>
+    <p><a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></p>
+<?php
+  }
+?>
   </article>
 <?php
   }
